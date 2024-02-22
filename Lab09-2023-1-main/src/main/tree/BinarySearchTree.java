b792609569f9df51d6ccdae6c84601bf374842b8
Gladistony Silva Lins <bistone3000@hotmail.com>
2024-02-22 16:55:37 -0300

    No da arvore e metodo de busca

--- Lab09-2023-1-main/src/main/tree/BinarySearchTree.java
+++ Lab09-2023-1-main/src/main/tree/BinarySearchTree.java
@@ -4,19 +4,61 @@ import estrut.Tree;
 
 public class BinarySearchTree implements Tree{
 
+    private NoArvore raiz;
+
     @Override
     public boolean buscaElemento(int valor) {
-        return true;
+        if (raiz == null) {
+            return false;
+        } else {
+            return recursivobuscaElemento(raiz, valor);
+        }
+    }
+
+    private boolean recursivobuscaElemento(NoArvore no,int valor) {
+        if (no == null) {
+            return false;
+        } else if (no.getValor() == valor) {
+            return true;
+        } else if (valor < no.getValor()) {
+            return recursivobuscaElemento(no.getEsquerda(), valor);
+        } else {
+            return recursivobuscaElemento(no.getDireita(), valor);
+        }
     }
 
     @Override
     public int minimo() {
-        return 0;
+        if (raiz == null) {
+            return 0;
+        } else {
+            return recursivominimo(raiz);
+        }
+    }
+
+    private int recursivominimo(NoArvore raiz2) {
+        if (raiz2.getEsquerda() == null) {
+            return raiz2.getValor();
+        } else {
+            return recursivominimo(raiz2.getEsquerda());
+        }
     }
 
     @Override
     public int maximo() {
-        return 0;
+        if (raiz == null) {
+            return 0;
+        } else {
+            return recursivomaximo(raiz);
+        }
+    }
+
+    private int recursivomaximo(NoArvore raiz2) {
+        if (raiz2.getDireita() == null) {
+            return raiz2.getValor();
+        } else {
+            return recursivomaximo(raiz2.getDireita());
+        }
     }
 
     @Override
---
+++ Lab09-2023-1-main/src/main/tree/NoArvore.java
@@ -0,0 +1,20 @@
+package tree;
+
+public class NoArvore {
+    private int valor;
+    private NoArvore esquerda;
+    private NoArvore direita;
+    private NoArvore pai;
+    private int altura;
+    
+    public int getValor() {
+        return valor;
+    }
+    public NoArvore getEsquerda() {
+        return esquerda;
+    }
+    public NoArvore getDireita() {
+        return direita;
+    }
+
+}
